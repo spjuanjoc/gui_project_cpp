69c9aa545b1629645debda2b37fa9cf2653f939f
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-08-18 18:54:41 -0500

    build(conan): add docopt
    - Handle the command-line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
+#include <docopt/docopt.h>
 #include <imgui-SFML.h>
 #include <imgui.h>
 #include <spdlog/spdlog.h>
@@ -12,6 +13,19 @@ constexpr auto frameRate{60};
 constexpr auto modeWidth{640};
 constexpr auto modeHeight{480};
 
+constexpr const char* USAGE =
+  R"(Game Project.
+
+    Usage:
+      game_project_cpp [options]
+
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+      --showWin     Show the window [default: false].
+      --width=<w>   Set screen width [default: 640].
+)";
+
 void foo()
 {
   spdlog::set_pattern("[%d-%m-%Y %T.%e %z] th[%t] %s:%!:%# [%l] %v");
@@ -21,11 +35,33 @@ void foo()
   SPDLOG_INFO("Some info message with extra details");
 }
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[])
+int main(int argc, const char* argv[])
 {
+  std::map<std::string, docopt::value> args{docopt::docopt(USAGE,
+                                                           {std::next(argv), std::next(argv, argc)},
+                                                           true,                        // show help if requested
+                                                           "game_project_cpp 0.0.1")};  // version string
+
+  for (auto const& arg : args)
+  {
+    if (arg.second.isBool())
+    {
+//      std::cout << arg.first << " = " << arg.second << " type" << typeid(arg.second).name() << '\n';
+      spdlog::info("{} = {}", arg.first, arg.second.asBool());
+    }
+    else if (arg.second.isString())
+    {
+      spdlog::info("{} = {}", arg.first, arg.second.asString());
+    }
+  }
+  const auto w = args["--width"].asLong();
+  const auto showW = args["--showWin"].asBool();
+  std::cout << "Width: " << w << ", show window: " << std::boolalpha << showW << '\n';
+
   spdlog::info("Hello spdlog");
   foo();
   spdlog::info(">>");
+
   sf::RenderWindow window(sf::VideoMode(modeWidth, modeHeight), "Window title");
   window.setFramerateLimit(frameRate);
   ImGui::SFML::Init(window);
@@ -37,8 +73,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[])
   ImGui::GetStyle().ScaleAllSizes(scaleFactor);
   ImGui::GetIO().FontGlobalScale = scaleFactor;
 
-  sf::Clock            deltaClock;
-  constexpr std::array options{"Option1", "Option2", "Option3"};
+  sf::Clock                        deltaClock;
+  constexpr std::array             options{"Option1", "Option2", "Option3"};
   std::array<bool, options.size()> states{};
 
   while (window.isOpen())
